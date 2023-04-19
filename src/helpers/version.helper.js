b11a218250d271773138327e4427d1e237ac72c9
Federico Grennon <fgrennon@steplix.com>
2023-04-19 17:01:19 -0300

    fixing tag validation

--- src/helpers/version.helper.js
+++ src/helpers/version.helper.js
@@ -8,7 +8,12 @@ exports.DEFAULT_VERSION = 0;
 
 exports.isValidTag = (tag) => {
     if (!tag) return false;
-    if (!prefixRegex.test(tag)) return false;
+    if (typeof tag !== 'string') return false;
+    const match = tag.match(prefixRegex);
+    if (!match) return false;
+    if (match.length !== 1) return false;
+    if (match[0] !== prefix) return false;
+
     const strVer = tag.replace(prefixRegex, '');
     const version = Number(strVer);
     return !isNaN(version);
